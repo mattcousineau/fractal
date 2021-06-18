1d8e8f823e0c1eba1d9cfe1efa1b52488f34fe03
mjcousineau1@gmail.com <mjcousineau1@gmail.com>
2021-06-18 14:32:45 -0400

    donation placeholders

--- contracts/Administration.sol
+++ contracts/Administration.sol
@@ -1,4 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.5;
 
-contract Administration {}
+contract Administration {
+    //change owner of contract
+    //add recepient to donation ledger
+    //remove recepient from donation ledger
+    //release and distribute funds
+    //check balance
+}
